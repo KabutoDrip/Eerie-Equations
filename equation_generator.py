9a88e7a873d3089c854a6a95dfe49c8a2c8a007f
KabutoDrip <cammiller42003@gmail.com>
2022-05-09 14:45:50 -0600

    rest of equations

--- equation_generator.py
+++ equation_generator.py
@@ -4,6 +4,10 @@ import random
 tier = 1
 var1,var2,var3,var4,ans = 0,0,0,0,0
 equation_set1 = [f"{var1} + {var2} = {ans}", f"{var1} - {var2} = {ans}"]
+equation_set2 = [f"{var1} + {var2} - {var3} = {ans}", f"{var1} - {var2} + {var3} = {ans}",f"{var1}"]
+equation_set3 = [f"{var1} x {var2} = {ans}", f"{var1} / {var2} = {ans}"]
+equation_set4 = [f"{var1} x {var2} = {ans}", f"{var1} / {var2} = {ans}"]
+equation_set5 = [f"{var1} x {var2} - {var3}", f"{var1} + {var2} - {var3} / {var4}"]
 
 def format_picker (tier):
   if (tier==1):
