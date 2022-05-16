8619ef7973de8e396878ca1417d61c6724807568
KabutoDrip <cammiller42003@gmail.com>
2022-05-16 14:11:45 -0600

    print debug

--- game/equation_generator.py
+++ game/equation_generator.py
@@ -1,10 +1,10 @@
 #Takes problem tier, picks an appropriate equation,
 #assigns variable values, returns equations and solutions to generated problem
 import random
-tier = 1 #variable pulled from game based off score, affects problem difficulty
+tier = 3 #variable pulled from game based off score, affects problem difficulty
 var1,var2,var3,var4,ans = 0,0,0,0,0
 equation_set1 = [f"{var1} + {var2} = {ans}", f"{var1} - {var2} = {ans}"]
-equation_set2 = [f"{var1} + {var2} - {var3} = {ans}", f"{var1} - {var2} + {var3} = {ans}",f"{var1}"]
+equation_set2 = [f"{var1} + {var2} - {var3} = {ans}", f"{var1} - {var2} + {var3} = {ans}"]
 equation_set3 = [f"{var1} x {var2} = {ans}", f"{var1} / {var2} = {ans}"]
 equation_set4 = [f"{var1} x {var2} = {ans}", f"{var1} / {var2} = {ans}"]
 equation_set5 = [f"{var1} x {var2} - {var3}", f"{var1} + {var2} - {var3} / {var4}"]
@@ -27,4 +27,4 @@ def format_picker (tier):
     return chosen_equation
   
   
-format_picker(tier)
+print(format_picker(tier))
