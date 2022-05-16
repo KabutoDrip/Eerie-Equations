b7765737a70ac14ceef88790100c8c3e328d9cc1
SlenderSlayer24 <86080078+SlenderSlayer24@users.noreply.github.com>
2022-05-16 15:01:20 -0600

    Added Variable generation

--- game/equation_generator.py
+++ game/equation_generator.py
@@ -1,16 +1,6 @@
 #Takes problem tier, picks an appropriate equation,
 #assigns variable values, returns equations and solutions to generated problem
 import random
-<<<<<<< HEAD
-=======
-tier = 3 #variable pulled from game based off score, affects problem difficulty
-var1,var2,var3,var4,ans = 0,0,0,0,0
-equation_set1 = [f"{var1} + {var2} = {ans}", f"{var1} - {var2} = {ans}"]
-equation_set2 = [f"{var1} + {var2} - {var3} = {ans}", f"{var1} - {var2} + {var3} = {ans}"]
-equation_set3 = [f"{var1} x {var2} = {ans}", f"{var1} / {var2} = {ans}"]
-equation_set4 = [f"{var1} x {var2} = {ans}", f"{var1} / {var2} = {ans}"]
-equation_set5 = [f"{var1} x {var2} - {var3} = {ans}", f"{var1} + {var2} - {var3} / {var4} = {ans}"]
->>>>>>> 39fe0842fc35b89706873f629af51dba123d5d21
 
 def main():
   tier = 5 #variable pulled from game based off score, affects problem difficulty
@@ -40,7 +30,6 @@ def format_picker (tier,var1,var2,var3,var4,ans):
     chosen_equation = random.choice(equation_set5)
     return chosen_equation
   
-<<<<<<< HEAD
 def variable_maker(tier):
     var1 = 0 
     var2 = 0
@@ -87,7 +76,3 @@ def variable_maker(tier):
       print("wot")
 
 main()
-=======
-  
-print(format_picker(tier))
->>>>>>> 39fe0842fc35b89706873f629af51dba123d5d21
